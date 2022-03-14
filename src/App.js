9371cabdfc0883e2ef4f2614414fceadd92f89d7
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-14 14:36:40 -0700

    fixed setter

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   useEffect(() => {
     axios.get(`https://denisse-app-backend.herokuapp.com/api/users/${userData.googleId}`)
     .then ((res) => {
-      setDataFromDB(res.data)
+      setDataFromDB(res.data[0])
     })
   }, [userData])
   console.log(dataFromDB)
