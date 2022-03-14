59611b8edfda2e2c517d70814c239ccd912c5c88
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-14 09:53:47 -0700

    deployment

--- src/appComponents/AppointmentCreate.js
+++ src/appComponents/AppointmentCreate.js
@@ -24,7 +24,7 @@ const AppointmentCreate = ({ userData }) => {
       formData.append("number", number)
       formData.append("date", appointmentDate)
       try {
-        const result = await axios.post('/api/appointments/createAppointment', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+        const result = await axios.post('https://denisse-app-backend.herokuapp.com/api/appointments/createAppointment', formData, { headers: {'Content-Type': 'multipart/form-data'}})
         return result.data 
       } catch(err) {
         alert(err)
--- src/appComponents/MyProfile.js
+++ src/appComponents/MyProfile.js
@@ -31,7 +31,7 @@ const MyProfile = ({ dataFromDB }) => {
         formData.append("image", image)
         formData.append("description", description)
         formData.append("type", type)
-        const result = await axios.post('/api/salonPhotos', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+        const result = await axios.post('https://denisse-app-backend.herokuapp.com/api/salonPhotos', formData, { headers: {'Content-Type': 'multipart/form-data'}})
         return result.data
       }
 
